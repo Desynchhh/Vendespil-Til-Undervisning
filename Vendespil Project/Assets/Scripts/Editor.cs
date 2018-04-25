de17204d826d2fc01dd12647adc1e8e453f24b80
LeakingPipes <madsbull1@hotmail.com>
2018-04-25 11:22:15 +0200

    Added editor script

---
+++ Vendespil Project/Assets/Scripts/Editor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine.UI;
+
+public class Editor : MonoBehaviour {
+
+    [Header("UI")]
+    public InputField QuestionText;
+    public InputField RightAswer;
+    public InputField WrongAnswer1;
+    public InputField WrongAnswer2;
+    public InputField WrongAnswer3;
+
+    [Header("MISC")]
+    public QuestionDatabase itemDB;
+
+    public void Button()
+    {
+        Question a = new Question();
+        a.IdNumber = 0;
+        a.question = QuestionText.text;
+        a.rightAnswer = RightAswer.text;
+        a.wrongAnswer1 = WrongAnswer1.text;
+        a.wrongAnswer2 = WrongAnswer2.text;
+        a.wrongAnswer3 = WrongAnswer3.text;
+
+        XmlSerializer serializer = new XmlSerializer(typeof(QuestionDatabase));
+        FileStream stream = new FileStream(Application.dataPath + "/XML/Data.xml", FileMode.Create);
+        serializer.Serialize(stream, itemDB);
+        itemDB.list.Add(a);
+        stream.Close();
+
+
+        //Add(QuestionText.text, RightAswer.text, WrongAnswer1.text, WrongAnswer2.text, WrongAnswer3.text);
+    }
+
+
+
+    [System.Serializable]
+    public class Question
+    {
+        public int IdNumber;
+        public string question;
+        public string rightAnswer;
+        public string wrongAnswer1;
+        public string wrongAnswer2;
+        public string wrongAnswer3;
+    }
+
+    [System.Serializable]
+    public class QuestionDatabase
+    {
+        public List<Question> list = new List<Question>();
+    }
+}
---
+++ Vendespil Project/Assets/Scripts/ShowXML.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class ShowXML : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject holder;
+    public QuestionDatabase itemDB;
+
+    public void LoadItems()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(QuestionDatabase));
+        FileStream stream = new FileStream(Application.dataPath + "/XML/Data.xml", FileMode.CreateNew);
+        itemDB = serializer.Deserialize(stream) as QuestionDatabase;
+        stream.Close();
+        Debug.Log(itemDB);
+
+        foreach (Question temp in itemDB.list)
+        {
+            Vector3 temppos = holder.transform.position;
+            GameObject go = Instantiate(prefab, temppos, Quaternion.identity) as GameObject;
+            go.transform.parent = holder.transform;
+        }
+    }
+
+
+    [System.Serializable]
+    public class Question
+    {
+        public int IdNumber;
+        public string question;
+        public string rightAnswer;
+        public string wrongAnswer1;
+        public string wrongAnswer2;
+        public string wrongAnswer3;
+    }
+
+    [System.Serializable]
+    public class QuestionDatabase
+    {
+        public List<Question> list = new List<Question>();
+    }
+}
