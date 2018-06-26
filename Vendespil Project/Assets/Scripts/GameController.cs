87809f8d6f74b8144ac3eb4eaf5ec19d090b3afc
Desynch <ltheguy939@gmail.com>
2018-06-26 09:06:21 +0200

    Added graphics. Game is missing particle effects & animations

--- Vendespil Project/Assets/Scripts/GameController.cs
+++ Vendespil Project/Assets/Scripts/GameController.cs
@@ -24,8 +24,8 @@ public class GameController : MonoBehaviour
     public GameObject contentEdit;
 
     [Header("Sorting Lists")]
-    public List<Transform> unorderedQuestions = new List<Transform>();
-    public List<Transform> orderedQuestions = new List<Transform>();
+    private List<Transform> unorderedQuestions = new List<Transform>();
+    private List<Transform> orderedQuestions = new List<Transform>();
     private string rightAnswer;
 
     [Header("Variables")]
@@ -34,20 +34,28 @@ public class GameController : MonoBehaviour
     private int totalAnswered;
     private int answeredRight;
     private int answeredWrong;
+    [HideInInspector]
     public bool hasOrderedQuestions;
 
-    [Header("Sprites")]
-    public Sprite black;
-    public Sprite white;
-    public Sprite red;
-    public Sprite green;
+    [Header("Question Sprites")]
+    public Sprite btnBlack;
+    public Sprite btnWhite;
+    public Sprite btnRed;
+    public Sprite btnGreen;
     public Sprite nextFinalAnswer;
     public Sprite nextCorrect;
     public Sprite nextWrong;
 
-    [Header("Colors")]
-    public Color txtBlack;
-    public Color txtWhite;
+    [Header("BG Overlay")]
+    public Transform bgOverlay;
+    public Sprite overlayBad;
+    public Sprite overlayGood;
+
+    [Header("Text Colors")]
+    private Color txtBlack;
+    private Color txtWhite;
+    private Color txtRed;
+    private Color txtGreen;
 
     [Header("Buttons")]
     public GameObject btnNext;
@@ -56,10 +64,26 @@ public class GameController : MonoBehaviour
     [Header("Results Field")]
     public GameObject rightAnswerScore;
 
+    [Header("Smiley")]
+    public Transform smiley;
+    private Image smileyFace;
+    private Transform smileyCannons;
+    private Transform smileyArms;
+    public Sprite score0;
+    public Sprite scoreLt50;
+    public Sprite score50;
+    public Sprite scoreGt50;
+    public Sprite score100;
+
     private void Start()
     {
+        smileyFace = smiley.Find("Face").GetComponent<Image>();
+        smileyCannons = smiley.Find("Cannons");
+        smileyArms = smiley.Find("Arms");
         txtBlack = new Color32(0x00, 0x00, 0x31, 0xFF);
         txtWhite = Color.white;
+        txtRed = new Color32(0xE6, 0x1E, 0x31, 0xFF);
+        txtGreen = new Color32(0x88, 0xD6, 0x0D, 0xFF);
     }
 
     public void SetQuestionsRandomOrder()
@@ -101,18 +125,20 @@ public class GameController : MonoBehaviour
             answeredRight = 0;
             answeredWrong = 0;
             totalAnswered = 0;
+            bgOverlay.gameObject.SetActive(false);
             FillFields();
             GetComponent<MenuManager>().PlayGame();
         }
     }
 
+    //OBS! SCRIPT WILL NOT SHOW NEXT QUESTION IF THE 'QUESTION' ELEMENT IS THE SAME AS THE LAST!
     public void FillFields()
     {
         btnNext.GetComponent<Image>().sprite = nextFinalAnswer;
         foreach (Transform child in parAnswers)
         {
             child.GetComponent<Button>().enabled = true;
-            child.GetComponent<Image>().sprite = black;
+            child.GetComponent<Image>().sprite = btnBlack;
             child.GetChild(0).GetComponent<Text>().color = txtWhite;
         }
         btnNext.GetComponent<Button>().enabled = false;
@@ -140,12 +166,12 @@ public class GameController : MonoBehaviour
         {
             if (child == selectedAnswer.transform)
             {
-                child.GetComponent<Image>().sprite = white;
+                child.GetComponent<Image>().sprite = btnWhite;
                 child.GetComponentInChildren<Text>().color = txtBlack;
             }
             else
             {
-                child.GetComponent<Image>().sprite = black;
+                child.GetComponent<Image>().sprite = btnBlack;
                 child.GetComponentInChildren<Text>().color = txtWhite;
             }
         }
@@ -156,21 +182,21 @@ public class GameController : MonoBehaviour
         if (selectedAnswer.GetComponentInChildren<Text>().text == rightAnswer)
         {
             answeredRight++;
-            selectedAnswer.GetComponent<Image>().sprite = green;
+            selectedAnswer.GetComponent<Image>().sprite = btnGreen;
             selectedAnswer.GetComponentInChildren<Text>().color = txtWhite;
             btnNext.GetComponent<Image>().sprite = nextCorrect;
         }
         else
         {
             answeredWrong++;
-            selectedAnswer.GetComponent<Image>().sprite = red;
+            selectedAnswer.GetComponent<Image>().sprite = btnRed;
             selectedAnswer.GetComponentInChildren<Text>().color = txtWhite;
             btnNext.GetComponent<Image>().sprite = nextWrong;
             foreach (Transform child in parAnswers)
             {
                 if (child.gameObject.GetComponentInChildren<Text>().text == rightAnswer && child.name != "Question")
                 {
-                    child.gameObject.GetComponent<Image>().sprite = green;
+                    child.gameObject.GetComponent<Image>().sprite = btnGreen;
                     selectedAnswer.GetComponentInChildren<Text>().color = txtWhite;
                 }
             }
@@ -191,6 +217,50 @@ public class GameController : MonoBehaviour
 
     public void WriteResults()
     {
+        if(answeredRight == 0)
+        {
+            smileyFace.sprite = score0;
+            smileyArms.gameObject.SetActive(true);
+            smileyCannons.gameObject.SetActive(false);
+            rightAnswerScore.GetComponentInChildren<Text>().color = txtRed;
+            bgOverlay.gameObject.SetActive(true);
+            bgOverlay.GetComponent<Image>().sprite = overlayBad;
+        }
+        else if (answeredRight == totalQuestions)
+        {
+            smileyFace.sprite = score100;
+            smileyArms.gameObject.SetActive(false);
+            smileyCannons.gameObject.SetActive(true);
+            rightAnswerScore.GetComponentInChildren<Text>().color = txtGreen;
+            bgOverlay.gameObject.SetActive(true);
+            bgOverlay.GetComponent<Image>().sprite = overlayGood;
+        }
+        else if (answeredRight == totalQuestions / 2)
+        {
+            smileyFace.sprite = score50;
+            smileyArms.gameObject.SetActive(false);
+            smileyCannons.gameObject.SetActive(false);
+            rightAnswerScore.GetComponentInChildren<Text>().color = txtBlack;
+            bgOverlay.gameObject.SetActive(false);
+        }
+        else if (answeredRight < totalQuestions / 2)
+        {
+            smileyFace.sprite = scoreLt50;
+            smileyArms.gameObject.SetActive(false);
+            smileyCannons.gameObject.SetActive(false);
+            rightAnswerScore.GetComponentInChildren<Text>().color = txtRed;
+            bgOverlay.gameObject.SetActive(true);
+            bgOverlay.GetComponent<Image>().sprite = overlayBad;
+        }
+        else if (answeredRight > totalQuestions / 2)
+        {
+            smileyFace.sprite = scoreGt50;
+            smileyArms.gameObject.SetActive(false);
+            smileyCannons.gameObject.SetActive(false);
+            rightAnswerScore.GetComponentInChildren<Text>().color = txtGreen;
+            bgOverlay.gameObject.SetActive(true);
+            bgOverlay.GetComponent<Image>().sprite = overlayGood;
+        }
         rightAnswerScore.GetComponentInChildren<Text>().text = string.Format("DU FIK\n {0} / {1}\n RIGTIGE!", answeredRight, totalQuestions);
     }
 
