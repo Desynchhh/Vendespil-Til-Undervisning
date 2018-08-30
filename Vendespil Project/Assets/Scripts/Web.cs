b60f15ee2555f8c97987063be0d2f2c7ba38b984
LeakingPipes <madsbull1@hotmail.com>
2018-08-30 10:02:28 +0200

    Added SimpleJSON

--- Vendespil Project/Assets/Scripts/Web.cs
+++ Vendespil Project/Assets/Scripts/Web.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using SimpleJSON;
 
-public class Web : MonoBehaviour {
+public class Web : MonoBehaviour
+{
+    [Header("Website address")]
+    public string URL = "http://localhost/getdata2.php";
 
-    [Header("UI")]
+    [Header("Interface")]
     public Button SendButton;
     public InputField Username;
     public InputField Password;
@@ -15,22 +19,20 @@ public class Web : MonoBehaviour {
         SendButton.onClick.AddListener(Send);
     }
 
-
     public void Send()
     {
         StartCoroutine(SendDataToPHP(Username.text, Password.text));
     }
 
-	IEnumerator SendDataToPHP(string Username, string Password)
+    IEnumerator SendDataToPHP(string Username, string Password)
     {
         WWWForm form = new WWWForm();
         form.AddField("Username", Username);
         form.AddField("Password", Password);
-
-        WWW www = new WWW("http://localhost/getdata2.php", form);
+        WWW www = new WWW(URL, form);
 
         yield return www;
-        if(www.error != null)
+        if (www.error != null)
         {
             Debug.Log("Ikke sendt!");
         }
@@ -39,6 +41,5 @@ public class Web : MonoBehaviour {
             Debug.Log(www.text);
             Debug.Log("Sendt!");
         }
-
     }
 }
